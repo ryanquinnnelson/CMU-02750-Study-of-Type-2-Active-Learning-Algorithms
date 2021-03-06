afe3ecb11d902ea44f27f898e05e834ab846a785
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-03-06 13:59:18 -0500

    Moved provide modules into helper

---
+++ packages/dh/dh.py
@@ -0,0 +1,107 @@
+import numpy as np
+import packages.dh.helper as helper
+
+def select_case_1(data, labels, T, budget, batch_size):
+    """DH algorithm where we choose P proportional to the size of subtree rooted at each node
+
+    :param data: Data matrix 1200x8
+    :param labels: true labels 1200x1
+    :param T: 3 element tree
+        T[0] = linkage matrix from hierarchical clustering.  See https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.linkage.html
+               for details. If you are unfamiliar with hierarchical clustering using scipy, the following is another helpful resource (We won't use dendrograms
+               here, but he gives a nice explanation of how to interpret the linkage matrix):
+               https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
+
+        T[1] = An array denoting the size of each subtree rooted at node i, where i indexes the array.
+               ie. The number of all children + grandchildren + ... + the node itself
+
+        T[2] = dict where keys are nodes and values are the node's parent
+    :param budget: Number of iterations to make
+    :param batch_size: Number of queries per iteration"""
+
+    n_nodes = len(T[1])  # total nodes in T
+    n_samples = len(data)  # total samples in data
+    L = np.zeros(n_nodes)  # majority label
+    p1 = np.zeros(n_nodes)  # empirical label frequency
+    n = np.zeros(n_nodes)  # number of points sampled from each node
+    error = []  # np.zeros(n_samples) #error at each round
+    root = n_nodes - 1  # corresponds to index of root
+    P = np.array([root])
+    L[root] = 1
+
+    for i in range(budget):
+        selected_P = []
+        for b in range(batch_size):
+            # TODO: select a node from P proportional to the size of subtree rooted at each node
+            raise NotImplementedError
+
+            ##TODO: pick a random leaf node from subtree Tv and query its label
+
+            # TODO: update empirical counts and probabilities for all nodes u on path from z to v
+
+        for p in selected_P:
+            # TODO: update admissible A and compute scores; find best pruning and labeling
+            raise NotImplementedError
+            # TODO: update pruning P and labeling L
+
+        # TODO: temporarily assign labels to every leaf and compute error
+        L_temp = L.copy()
+        raise NotImplementedError
+
+    for i in range(len(P)):
+        L = helper.assign_labels(L, P[i], P[i], T, n_samples)
+
+    return L, np.array(error)
+
+
+def select_case_2(data, labels, T, budget, batch_size):
+    """DH algorithm where we choose P by biasing towards choosing nodes in areas where the observed labels are less pure
+
+    :param data: Data matrix 1200x8
+    :param labels: true labels 284x1
+    :param T: 3 element tree
+        T[0] = linkage matrix from hierarchical clustering.  See https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.linkage.html
+               for details. If you are unfamiliar with hierarchical clustering using scipy, the following is another helpful resource (We won't use dendrograms
+               here, but he gives a nice explanation of how to interpret the linkage matrix):
+               https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
+
+        T[1] = An array denoting the size of each subtree rooted at node i, where i indexes the array.
+               ie. The number of all children + grandchildren + ... + the node itself
+
+        T[2] = dict where keys are nodes and values are the node's parent
+    :param budget: Number of iterations to make
+    :param batch_size: Number of queries per iteration"""
+
+    n_nodes = len(T[1])  # total nodes in T
+    n_samples = len(data)  # total samples in data
+    L = np.zeros(n_nodes, dtype=int)  # majority label
+    p1 = np.zeros(n_nodes)  # empirical label frequency
+    n = np.zeros(n_nodes)  # number of points sampled from each node
+    error = []  # np.zeros(n_samples) #error at each round
+    root = n_nodes - 1  # corresponds to index of root
+    P = np.array([root])
+    L[root] = 1
+
+    for i in range(budget):
+        selected_P = []
+        for b in range(batch_size):
+            # TODO: select a node from P biasing towards choosing nodes in areas where the observed labels are less pure
+            raise NotImplementedError
+
+            # TODO: pick a random leaf node from subtree Tv and query its label
+
+            # TODO: update empirical counts and probabilities for all nodes u on path from z to v
+
+        for p in selected_P:
+            # TODO: update admissible A and compute scores; find best pruning and labeling
+            raise NotImplementedError
+            # TODO: update pruning P and labeling L
+
+        # TODO: temporarily assign labels to every leaf and compute error
+        L_temp = L.copy()
+        raise NotImplementedError
+
+    for i in range(len(P)):
+        L = helper.assign_labels(L, P[i], P[i], T, n_samples)
+
+    return L, np.array(error)
--- packages/dh/helper.py
+++ packages/dh/helper.py
@@ -93,6 +93,7 @@ def load_data(filename, seed, filter_class):
     return X_train, y_train, X_test, y_test, T
 
 
+# I created
 def get_classifier(choice, X=None, y=None, seed=None):
     model = None
 
@@ -172,3 +173,173 @@ def get_classifier(choice, X=None, y=None, seed=None):
         model = nn
 
     return model
+
+
+# provided
+def compute_error(L, labels):
+    """
+    Compute the error
+
+    :param L: labeling of leaf nodes
+    :param labels: true labels of each node
+
+    :return: error of predictions
+    """
+
+    wrong = 0
+    wrong = (L[:len(labels)] != labels).sum()
+    error = wrong / len(labels)
+    return error
+
+
+def assign_labels(L, u, v, T, n_samples):
+    """Assign labels to every leaf according to the label of the subtree's root node
+
+    :param L: array of predicted labels for each node
+    :param u: current node
+    :param v: subtree's root node
+    :param T: tree- 3 element list, see dh.py for description
+    :param nsample: number of samples, 1000
+
+    :returns L: array of predicted label for each node
+    """
+
+    link = T[0]
+    if u < n_samples:
+        L[u] = L[v]
+        return L
+    else:
+        left = link[u - n_samples, 0]
+        L = assign_labels(L, left, v, T, n_samples)
+        right = link[u - n_samples, 1]
+        L = assign_labels(L, right, v, T, n_samples)
+        return L
+
+
+def best_pruning_and_labeling(n, p1, v, T, n_samples):
+    """ update admissible A and compute scores; find best pruning and labeling
+
+    :param n: array with number of points sampled in the subtree rooted at each node
+    :param p1: array with fraction of label = 1 in the subtree rooted at each node
+    :param v: selected subtree Tv's root node
+    :param T: tree- 3 element list, see dh.py for description
+    :param n_samples: number of samples, 1000
+
+    :returns P_best: the best new pruning
+    :returns L_best: the best labeling for v
+    """
+    p0_tmp = 1 - p1 - ((1 / n) + np.sqrt(p1 * (1 - p1) / n))
+    p0_LB = np.fmax(p0_tmp, 0)  # fmax is componentwise and ignores nans, unlike max
+
+    p1_tmp = p1 - ((1 / n) + np.sqrt(p1 * (1 - p1) / n))
+    p1_LB = np.fmax(p1_tmp, 0)
+
+    A0 = p0_LB > 1 / 3
+    A1 = p1_LB > 1 / 3
+    e1 = 1 - p1
+    e1_tilde = np.ones(len(e1))
+    e1_tilde[A1] = e1[A1]
+    e0 = p1
+    e0_tilde = np.ones(len(e0))
+    e0_tilde[A0] = e1[A0]
+
+    score0 = np.full_like(n, np.nan)
+    score1 = np.full_like(n, np.nan)
+    score = np.zeros(len(n))
+
+    link = T[0]
+    sizes_of_subtrees = T[1]
+    parents = T[2]
+
+    for i in range(len(n)):
+        parent = parents[i]
+        if parent == 0:
+            break
+        if A0[i]:
+            if np.isnan(score0[parent]):
+                score0[parent] = sizes_of_subtrees[i] / sizes_of_subtrees[parent] * score[i]
+            else:
+                score0[parent] += sizes_of_subtrees[i] / sizes_of_subtrees[parent] * score[i]
+        if A1[i]:
+            if np.isnan(score1[parent]):
+                score1[parent] = sizes_of_subtrees[i] / sizes_of_subtrees[parent] * score[i]
+            else:
+                score1[parent] += sizes_of_subtrees[i] / sizes_of_subtrees[parent] * score[i]
+        possible_scores_tmp = [score0[i], score1[i], e0_tilde[i], e1_tilde[i]]
+        score[i] = np.nanmin(possible_scores_tmp)
+
+    possible_scores_tmp = [score0[-1], score1[-1], e0_tilde[-1], e1_tilde[-1]]
+    score[-1] = np.nanmin(possible_scores_tmp)
+
+    scores_tmp = [score0[v], score1[v], e0_tilde[v], e1_tilde[v]]
+    best = np.nanargmin(scores_tmp)
+
+    if best == 0:
+        L_best = 0
+        if v < n_samples:
+            P_best = np.array([v])
+        else:
+            left = link[v - n_samples, 0]
+            right = link[v - n_samples, 1]
+            P_best = np.array([left, right])
+    elif best == 1:
+        L_best = 1
+        if v < n_samples:
+            P_best = np.array([v])
+        else:
+            left = link[v - n_samples, 0]
+            right = link[v - n_samples, 1]
+            P_best = np.array([left, right])
+    elif best == 2:
+        L_best = 0
+        P_best = np.array([v])
+    else:
+        L_best = 1
+        P_best = np.array([v])
+
+    return P_best, L_best
+
+
+def get_leaves(leaves, v, T, n_samples):
+    """Get all leaf nodes in the subtree Tv rooted at v
+
+    :param leaves: previously found leaves
+    :param v: current root node
+    :param T: Tree- 3 element list, see dh.py for description
+    :param n_samples: number of samples
+
+    :returns leaves: leaves in the subtree Tv rooted at v"""
+
+    link = T[0]
+    if v < n_samples:
+        leaves.append(v)
+        return leaves
+    else:
+        left = link[v - n_samples, 0]
+        leaves = get_leaves(leaves, left, T, n_samples)
+        right = link[v - n_samples, 1]
+        leaves = get_leaves(leaves, right, T, n_samples)
+        return leaves
+
+
+def update_empirical(n, p1, v, z, l, T):
+    ''' Update empirical counts and probabilities for all nodes u on path
+
+    :param n: array with number of points sampled in the subtree rooted at each node
+    :param p1: array with fraction of label = 1 in the subtree rooted at each node
+    :param v: selected subtree Tv's root node
+    :param z: selected leaf node in subtree Tv
+    :param l: queried label for node z
+    :param T: tree- 3 element list, see dh.py for details
+
+    :returns n: array with updated number of points sampled in the subtree rooted at each node
+    :returns p1: array with updated fraction of label = 1 in the subtree rooted at each node
+    '''
+
+    parents = T[2]
+    while z <= v and z != 0:
+        l1 = n[z] * p1[z]
+        n[z] += 1
+        p1[z] = (l1 + l) / n[z]
+        z = np.array([parents[int(z)]])
+    return n, p1
