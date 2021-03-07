4c4496a90df9d0454d8fc41b2600ae6cb360c202
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-03-06 22:41:50 -0500

    Working through case 1:

--- packages/dh/dh.py
+++ packages/dh/dh.py
@@ -1,70 +1,76 @@
 import numpy as np
 import packages.dh.helper as helper
+import random
 
 
 def select_case_1(X, y_true, T, budget, batch_size):
-    return np.array([np.random.randint(0,2) for i in range(len(T[1]))]), [np.random.uniform(0,1) for i in range(budget)]
+    """
 
+    :param X: a x b matrix, data samples
+    :param y_true: a x 1 vector, true labels for samples
+    :param T: Tree data structure
+    :param budget: Number of iterations allowed
+    :param batch_size: Number of queries per iteration
+    :return:
+    """
 
-def select_case_2(X, y_true, T, budget, batch_size):
-    return np.array([np.random.randint(0,2) for i in range(len(T[1]))]), [np.random.uniform(0,1) for i in range(budget)]
+    # define variables
+    num_nodes = len(T[1])  # total nodes in T
+    num_samples = len(X)  # total samples in data
 
-# def select_case_1(X, y_true, T, budget, batch_size):
-#     """
-#
-#     :param X: a x b matrix, data samples
-#     :param y_true: a x 1 vector, true labels for samples
-#     :param T: Tree data structure
-#     :param budget: Number of iterations allowed
-#     :param batch_size: Number of queries per iteration
-#     :return:
-#     """
-#
-#     # define variables
-#     num_nodes = len(T[1])  # total nodes in T
-#     num_samples = len(X)  # total samples in data
-#
-#     # set scaffolds to fill in
-#     L = np.zeros(num_nodes)  # majority label
-#     pHat1 = np.zeros(num_nodes)  # empirical label frequency
-#     n = np.zeros(num_nodes)  # number of points sampled from each node
-#
-#     # additional
-#     error = []  # error for each iteration
-#
-#     # set initial pruning and labeling for root
-#     root = num_nodes - 1  # index of root
-#     P = np.array([root])
-#     L[root] = 1
-#
-#     for i in range(budget):
-#
-#         selected_P = []  # pruning after each query in this iteration
-#         for b in range(batch_size):
-#         # TODO: select a node from P proportional to the size of subtree rooted at each node
-#
-#         # TODO: pick a random leaf node from subtree Tv and query its label
-#
-#         # TODO: update empirical counts and probabilities for all nodes u on path from z to v
-#
-#         for p in selected_P:
-#         # TODO: update admissible A and compute scores; find best pruning and labeling
-#
-#         # TODO: update pruning P and labeling L
-#
-#         # ?? start from root?
-#         # TODO: temporarily assign labels to every leaf and compute error
-#         L_temp = L.copy()
-#         L_i = helper.assign_labels(L_temp, root, root, T, num_samples)
-#         error_i = helper.compute_error(L_i, y_true)
-#         error.append(error_i)
-#
-#     # assign final labeling based on best pruning
-#     for i in range(len(P)):
-#         L = helper.assign_labels(L, P[i], P[i], T, num_samples)
-#
-#     return L, np.array(error)
+    # set scaffolds to fill in
+    n = np.zeros(num_nodes)  # number of points sampled from each node
+    pHat1 = np.zeros(num_nodes)  # empirical label frequency
+    L = np.zeros(num_nodes)  # majority label
 
+    # additional
+    error = []  # error for each iteration
+
+    # set initial pruning and labeling for root
+    root = num_nodes - 1  # index of root
+    P = np.array([root])
+    L[root] = 1
+
+    for i in range(budget):
+
+        # ??
+        selected_P = []  # pruning after each query in this iteration
+        for b in range(batch_size):
+            pass
+            # TODO: select a node from P proportional to the size of subtree rooted at each node
+            v = 1
+
+            # TODO: pick a random leaf node from subtree Tv and query its label (DONE)
+            v_leaves = helper.get_leaves([], v, T, num_samples)
+            z = random.choice(v_leaves)
+            label_z = y_true[z]
+
+            # TODO: update empirical counts and probabilities for all nodes u on path from z to v (DONE)
+            n, pHat1 = helper.update_empirical(n, pHat1, v, z, label_z, T)
+
+        # ??
+        for p in selected_P:
+            # TODO: update admissible A and compute scores; find best pruning and labeling
+            # ?? what should v be?
+            P_best, L_best = helper.best_pruning_and_labeling(n, pHat1, P[p], T, num_samples)
+
+            # TODO: update pruning P and labeling L
+            # ?? store on its own? what does it mean to update labeling and pruning P?
+            P = P_best
+            L = helper.assign_labels(L, P[p], P[p], T, num_samples)
+
+        # ?? start from root? what should v be?
+        # TODO: temporarily assign labels to every leaf and compute error
+        L_temp = L.copy()
+        L_i = helper.assign_labels(L_temp, P[i], P[i], T, num_samples)
+        error_i = helper.compute_error(L_i, y_true)
+        error.append(error_i)
+
+    # assign final labeling based on best pruning
+    for i in range(len(P)):
+        L = helper.assign_labels(L, P[i], P[i], T, num_samples)
+
+    return L, np.array(error)
 
 # def select_case_2(data, labels, T, budget, batch_size):
 #     """DH algorithm where we choose P by biasing towards choosing nodes in areas where the observed labels are less pure
@@ -119,4 +125,10 @@ def select_case_2(X, y_true, T, budget, batch_size):
 #     return L, np.array(error)
 
 
-
+# for testing purposes only
+# def select_case_1(X, y_true, T, budget, batch_size):
+#     return np.array([np.random.randint(0,2) for i in range(len(T[1]))]), [np.random.uniform(0,1) for i in range(budget)]
+#
+#
+# def select_case_2(X, y_true, T, budget, batch_size):
+#     return np.array([np.random.randint(0,2) for i in range(len(T[1]))]), [np.random.uniform(0,1) for i in range(budget)]
